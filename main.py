42339b34e1100992cc3c792777f08e05f208ab31
simsieec025 <simsieec025@student.wethinkcode.co.za>
2026-02-03 00:59:04 +0200

    making my thought

---
+++ main.py
@@ -0,0 +1,9 @@
+import json
+
+class StudentPerformance:
+    def __init__(self,name, surname , marks):
+        self.names = name
+        self.surnames = surname
+        self.marks = marks
+        
+    
